ff5fd29f385189da5d2c206762341b9b6a9d249f
admin <kdv3474@gmail.com>
2025-10-30 11:27:52 +0500

    изменение test_code.py

--- tests/test_code.py
+++ tests/test_code.py
@@ -5,4 +5,3 @@ from scr.masks import get_mask_card_number
 def test_get_mask_card_number(sample_data):
     number_card, mask_new = sample_data
     assert get_mask_card_number(number_card) == mask_new
-
